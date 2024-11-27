03aec4c731c4d39243cb0bc6d772fd7814fb31e3
chip <162468957+chippokiddo@users.noreply.github.com>
2024-11-27 08:29:01 +0800

    Refactor readerApp and CheckForUpdates

--- reader/Utils/CheckForUpdates.swift
+++ reader/Utils/CheckForUpdates.swift
@@ -33,57 +33,10 @@ func fetchLatestRelease() async throws -> (String, URL) {
     
     // Check if there are assets available
     guard let downloadURL = release.assets.first?.browserDownloadURL else {
-        // Print the raw JSON response for debugging purposes
-        if let jsonString = String(data: data, encoding: .utf8) {
-            print("JSON Response: \(jsonString)")
-        }
         throw NSError(domain: "GitHubReleaseError", code: 0, userInfo: [
             NSLocalizedDescriptionKey: "No assets available in the latest release on GitHub."
         ])
     }
     
     return (release.tagName, downloadURL)
-}
-
-// Main function to check for updates, now prompting the user if an update is available
-func checkForUpdates() {
-    Task { @MainActor in
-        do {
-            let (latestVersion, downloadURL) = try await fetchLatestRelease()
-            let currentVersion = Bundle.main.infoDictionary?["CFBundleShortVersionString"] as? String
-            
-            if currentVersion != latestVersion {
-                print("New update available")
-                
-                let userWantsToUpdate = await promptForUpdate(latestVersion: latestVersion)
-                
-                if userWantsToUpdate {
-                    print("User opted to update")
-                    
-                    // Open the browser to the GitHub release downloads page
-                    NSWorkspace.shared.open(downloadURL)
-                } else {
-                    print("User declined the update")
-                }
-            } else {
-                print("Already on the latest version")
-            }
-        } catch {
-            print("Failed to check for updates: \(error.localizedDescription)")
-        }
-    }
-}
-
-// Function to prompt the user for an update, now marked as async and @MainActor
-@MainActor
-func promptForUpdate(latestVersion: String) async -> Bool {
-    let alert = NSAlert()
-    alert.messageText = "New Update Available"
-    alert.informativeText = "reader \(latestVersion) is available. Would you like to download it?"
-    alert.alertStyle = .informational
-    alert.addButton(withTitle: "Download")
-    alert.addButton(withTitle: "Cancel")
-    
-    let response = alert.runModal()
-    return response == .alertFirstButtonReturn // Returns true if "Download" is clicked
 }
--- reader/readerApp.swift
+++ reader/readerApp.swift
@@ -4,12 +4,11 @@ import SwiftData
 @main
 struct readerApp: App {
     private static let sharedModelContainer = createModelContainer()
-
+    
     @StateObject private var appState = AppState()
     @StateObject private var dataManager: DataManager
     @Environment(\.openWindow) private var openWindow
-
-    // State variables for the update functionality
+    
     @State private var isCheckingForUpdates = false
     @State private var latestVersion: String = ""
     @State private var downloadURL: URL?
@@ -18,55 +17,35 @@ struct readerApp: App {
     @AppStorage("checkForUpdatesAutomatically") private var checkForUpdatesAutomatically: Bool = false
     
     @NSApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
-
+    
     init() {
         guard let container = readerApp.sharedModelContainer else {
             fatalError("ModelContainer is not available.")
         }
         _dataManager = StateObject(wrappedValue: DataManager(modelContainer: container))
     }
-
+    
     var body: some Scene {
+        mainWindow
+        aboutWindow
+        settingsWindow
+        addBookWindow
+    }
+    
+    // MARK: - Main Window
+    private var mainWindow: some Scene {
         WindowGroup {
-            if let container = readerApp.sharedModelContainer {
-                ContentView(viewModel: ContentViewModel(dataManager: dataManager))
-                    .environmentObject(appState)
-                    .environmentObject(dataManager)
-                    .environment(\.modelContainer, container)
-                    .alert(item: $alertType) { alertType in
-                        switch alertType {
-                        case .newUpdateAvailable:
-                            return Alert(
-                                title: Text("New Update Available"),
-                                message: Text("reader \(latestVersion) is available. Would you like to download it?"),
-                                primaryButton: .default(Text("Download")) {
-                                    if let downloadURL = downloadURL {
-                                        NSWorkspace.shared.open(downloadURL) // Open the browser to the download URL
-                                    }
-                                },
-                                secondaryButton: .cancel(Text("Later"))
-                            )
-                        case .upToDate:
-                            return Alert(
-                                title: Text("No Updates Available"),
-                                message: Text("You are already on the latest version."),
-                                dismissButton: .default(Text("OK"))
-                            )
-                        }
-                    }
-                    .onAppear {
-                        appState.applyTheme(appState.selectedTheme)
-                        scheduleDailyUpdateCheck()
-                    }
-                    .onDisappear {
-                        NSApp.terminate(nil)
-                    }
-            } else {
-                Text("Failed to initialize data model")
-                    .onAppear {
-                        print("Warning: ModelContainer failed to initialize.")
-                    }
-            }
+            ContentView(viewModel: ContentViewModel(dataManager: dataManager))
+                .environmentObject(appState)
+                .environmentObject(dataManager)
+                .environment(\.modelContainer, readerApp.sharedModelContainer!)
+                .alert(item: $alertType, content: createAlert)
+                .onAppear {
+                    handleOnAppear()
+                }
+                .onDisappear {
+                    NSApp.terminate(nil)
+                }
         }
         .commands {
             CommandGroup(replacing: .appInfo) {
@@ -85,20 +64,29 @@ struct readerApp: App {
                 }
             }
         }
-
+    }
+    
+    // MARK: - About Window
+    private var aboutWindow: some Scene {
         Window("About reader", id: "aboutWindow") {
             AboutView()
         }
         .windowStyle(HiddenTitleBarWindowStyle())
         .windowResizability(.contentSize)
-        
+    }
+    
+    // MARK: - Settings Window
+    private var settingsWindow: some Scene {
         Window("Settings", id: "settingsWindow") {
             SettingsView()
                 .environmentObject(appState)
         }
         .windowStyle(HiddenTitleBarWindowStyle())
         .windowResizability(.contentSize)
-        
+    }
+    
+    // MARK: - Add Book Window
+    private var addBookWindow: some Scene {
         Window("Add Book", id: "addBookWindow") {
             AddView()
                 .environmentObject(dataManager)
@@ -106,6 +94,47 @@ struct readerApp: App {
         }
         .windowStyle(HiddenTitleBarWindowStyle())
         .windowResizability(.contentSize)
+    }
+    
+    // MARK: - Update Alerts
+    private func createAlert(for alertType: AlertType) -> Alert {
+        switch alertType {
+        case .newUpdateAvailable:
+            return Alert(
+                title: Text("New Update Available"),
+                message: Text("reader \(latestVersion) is available. Would you like to download it?"),
+                primaryButton: .default(Text("Download")) {
+                    if let downloadURL = downloadURL {
+                        NSWorkspace.shared.open(downloadURL)
+                    }
+                },
+                secondaryButton: .cancel(Text("Later"))
+            )
+        case .upToDate:
+            return Alert(
+                title: Text("No Updates Available"),
+                message: Text("You are already on the latest version."),
+                dismissButton: .default(Text("OK"))
+            )
+        }
+    }
+    
+    // MARK: - Helper Functions
+    private func handleOnAppear() {
+        appState.applyTheme(appState.selectedTheme)
+        scheduleDailyUpdateCheck()
+    }
+    
+    private func scheduleDailyUpdateCheck() {
+        guard checkForUpdatesAutomatically else { return }
+        
+        let lastUpdateCheck = UserDefaults.standard.object(forKey: "lastUpdateCheck") as? Date ?? .distantPast
+        let now = Date()
+        
+        if !Calendar.current.isDate(now, inSameDayAs: lastUpdateCheck) {
+            checkForAppUpdates(isUserInitiated: false)
+            UserDefaults.standard.set(now, forKey: "lastUpdateCheck")
+        }
     }
     
     private static func createModelContainer() -> ModelContainer? {
@@ -117,60 +146,40 @@ struct readerApp: App {
             return nil
         }
     }
-
-    // Function to check for updates, showing an alert with the update status
+    
+    // Consolidated Update Check
     private func checkForAppUpdates(isUserInitiated: Bool) {
         isCheckingForUpdates = true
-
+        
         Task {
             do {
                 let (latestVersionFound, downloadURLFound) = try await fetchLatestRelease()
                 let currentVersion = Bundle.main.infoDictionary?["CFBundleShortVersionString"] as? String ?? "0.0.0"
-
+                
                 if isNewerVersion(latestVersionFound, than: currentVersion) {
                     latestVersion = latestVersionFound
                     downloadURL = downloadURLFound
                     alertType = .newUpdateAvailable
                 } else if isUserInitiated {
-                    // Only show "up to date" alert if the check was initiated by the user
                     alertType = .upToDate
                 }
             } catch {
                 print("Failed to check for updates: \(error.localizedDescription)")
             }
-
+            
             isCheckingForUpdates = false
         }
     }
-
-
-    // Helper function to compare version strings
-    func isNewerVersion(_ newVersion: String, than currentVersion: String) -> Bool {
+    
+    private func isNewerVersion(_ newVersion: String, than currentVersion: String) -> Bool {
         let newComponents = newVersion.split(separator: ".").compactMap { Int($0) }
         let currentComponents = currentVersion.split(separator: ".").compactMap { Int($0) }
-
+        
         for (new, current) in zip(newComponents, currentComponents) {
-            if new > current {
-                return true // newVersion is greater
-            } else if new < current {
-                return false // newVersion is not greater
-            }
+            if new > current { return true }
+            if new < current { return false }
         }
-
-        // If all components so far are equal, but newVersion has more components (e.g., "2.1.1" vs "2.1")
+        
         return newComponents.count > currentComponents.count
-    }
-    
-    private func scheduleDailyUpdateCheck() {
-        guard checkForUpdatesAutomatically else { return }
-
-        let lastUpdateCheck = UserDefaults.standard.object(forKey: "lastUpdateCheck") as? Date ?? .distantPast
-        let now = Date()
-
-        // If the last update check was more than a day ago
-        if Calendar.current.isDate(now, inSameDayAs: lastUpdateCheck) == false {
-            checkForAppUpdates(isUserInitiated: false)
-            UserDefaults.standard.set(now, forKey: "lastUpdateCheck")
-        }
     }
 }
